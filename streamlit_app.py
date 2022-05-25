98450c198e68c4ce17da990f598a557a1235cb20
Ashwini Shingare <106136295+AshwiniShingare20@users.noreply.github.com>
2022-05-25 11:22:51 +0530

    Update streamlit_app.py
    Created another header for a special menu item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My parents New Healthy diner')
+streamlit.title('My Mom's New Healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Bleuberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
