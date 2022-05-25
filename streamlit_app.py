343ada1b2f8cee5caba7e5e2941365be53856396
Ashwini Shingare <106136295+AshwiniShingare20@users.noreply.github.com>
2022-05-25 11:13:35 +0530

    Update streamlit_app.py
    added header
    added 3 menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Bleuberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+stramlt.text('Hard-Boiled Free-Range Egg')
